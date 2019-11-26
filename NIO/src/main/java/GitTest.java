8f4874d6f85c5599ce307913a9ab35f1857ae957
Gallant Huang <405786972@qq.com>
2019-11-26 14:59:26 +0800

    server

--- NIO/src/main/java/GitTest.java
+++ NIO/src/main/java/GitTest.java
@@ -1,11 +1,11 @@
 /**
  *
- * haha
+ * haha from server
  * Created by huangjunhao on 19/11/26.
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("hello, are u ok zhoujielun");
         System.out.println("git modified by git server.");
     }
 }
