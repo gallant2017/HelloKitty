4a3563de08f5d003fbd98c4f690a59d452d5c2e8
huangjunhao <huangjunhao@autohome.com.cn>
2019-12-26 23:49:16 +0800

    1

---
+++ part1/src/main/java/com/gallant/thread/ReentrantLockDemo.java
@@ -0,0 +1,42 @@
+package com.gallant.thread;
+
+
+import com.gallant.common.Print;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ReentrantLock 重入锁
+ *
+ * lock 获取锁 (可以多次获得,但也需要多次释放,否则其它线程将无法进入临界区)
+ * unlock 释放锁
+ * Created by huangjunhao on 19/12/26.
+ */
+public class ReentrantLockDemo {
+
+    static ReentrantLock lock = new ReentrantLock();
+    static int i = 0;
+
+    public static class R1 implements Runnable {
+
+        @Override
+        public void run() {
+            for (int j = 0; j < 1000000; j++) {
+                lock.lock();
+                i++;
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        R1 r1 = new R1();
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r1);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        Print.Info("i=" + i);
+    }
+}
