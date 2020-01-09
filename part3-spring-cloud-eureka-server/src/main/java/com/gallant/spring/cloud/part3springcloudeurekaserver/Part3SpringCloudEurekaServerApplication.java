6d6ce6ee9fc9a91ca2b88cb7bc7f51dc4d42e180
huangjunhao <huangjunhao@autohome.com.cn>
2020-01-09 15:49:14 +0800

    '1'

--- part3-spring-cloud-eureka-server/src/main/java/com/gallant/spring/cloud/part3springcloudeurekaserver/Part3SpringCloudEurekaServerApplication.java
+++ part3-spring-cloud-eureka-server/src/main/java/com/gallant/spring/cloud/part3springcloudeurekaserver/Part3SpringCloudEurekaServerApplication.java
@@ -4,6 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Eurka 工作流程
+ * 了解完 Eureka 核心概念，自我保护机制，以及集群内的工作原理后，我们来整体梳理一下 Eureka 的工作流程：
+ * 1、Eureka Server 启动成功，等待服务端注册。在启动过程中如果配置了集群，集群之间定时通过 Replicate 同步注册表，每个 Eureka Server 都存在独立完整的服务注册表信息
+ * 2、Eureka Client 启动时根据配置的 Eureka Server 地址去注册中心注册服务
+ * 3、Eureka Client 会每 30s 向 Eureka Server 发送一次心跳请求，证明客户端服务正常
+ * 4、当 Eureka Server 90s 内没有收到 Eureka Client 的心跳，注册中心则认为该节点失效，会注销该实例
+ * 5、单位时间内 Eureka Server 统计到有大量的 Eureka Client 没有上送心跳，则认为可能为网络异常，进入自我保护机制，不再剔除没有上送心跳的客户端
+ * 6、当 Eureka Client 心跳请求恢复正常之后，Eureka Server 自动退出自我保护模式
+ * 7、Eureka Client 定时全量或者增量从注册中心获取服务注册表，并且将获取到的信息缓存到本地
+ * 8、服务调用时，Eureka Client 会先从本地缓存找寻调取的服务。如果获取不到，先从注册中心刷新注册表，再同步到本地缓存
+ * 9、Eureka Client 获取到目标服务器信息，发起服务调用
+ * 10、Eureka Client 程序关闭时向 Eureka Server 发送取消请求，Eureka Server 将实例从注册表中删除
+ * 这就是Eurka基本工作流程
+ *
+ * ==================================总结 ===============================
+ * 讲了 Eureka 核心概念、Eureka 自我保护机制和 Eureka 集群原理。通过分析 Eureka 工作原理，我可以明显地感觉到 Eureka 的设计之巧妙，通过一些列的机制，完美地解决了注册中心的稳定性和高可用性。
+ * Eureka 为了保障注册中心的高可用性，容忍了数据的非强一致性，服务节点间的数据可能不一致， Client-Server 间的数据可能不一致。比较适合跨越多机房、对注册中心服务可用性要求较高的使用场景。
+ * 架构图见 eureka.png
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class Part3SpringCloudEurekaServerApplication {
